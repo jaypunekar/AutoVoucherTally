dfa4548b912e0ab92a6bb7682b07b33fb09e4ec2
Jay Punekar <jaypunekar01@gmail.com>
2023-07-03 23:22:39 +0530

    added util.py

--- util.py
+++ util.py
@@ -1,4 +1,25 @@
 import customtkinter
+from CTkMessagebox import CTkMessagebox
+from pymongo.mongo_client import MongoClient
+from tkinter import ttk
+from datetime import datetime
+import pandas as pd
+from PIL import Image
+import requests
+import xml.etree.ElementTree as Et
+
+
+# Tally URL
+tally_url = "https://localhost:9000"
+
+# MongoDB Atlas URL
+mongo_url = "mongodb+srv://mongodb:mongodb@tally.i6wfrlt.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(mongo_url)
+
+# Database Name and Collection Name
+db = client['Punekar']
+collec = db['Tally']
 
 class UpdateFrame(customtkinter.CTkFrame):
     def __init__(self, resort_name, amount, reason, master, **kwargs):
@@ -31,3 +52,112 @@ class UpdateFrame(customtkinter.CTkFrame):
 
         self.button_update = customtkinter.CTkButton(self, text="Update",)
         self.button_update.place(relx=0.7, rely=0.90, anchor='s')
+
+
+
+class ButtonFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.update_button = customtkinter.CTkButton(self, text="View")
+        self.update_button.pack(pady=10)
+        self.update_button = customtkinter.CTkButton(self, text="Delete")
+        self.update_button.pack(pady=10)
+        self.update_button = customtkinter.CTkButton(self, text="Approve")
+        self.update_button.pack(pady=10)
+        self.update_button = customtkinter.CTkButton(self, text="Add Resort")
+        self.update_button.pack(pady=10)
+        self.update_button = customtkinter.CTkButton(self, text="Check Resort")
+        self.update_button.pack(pady=10)
+        self.update_button = customtkinter.CTkButton(self, text="Add to Tally", fg_color='red')
+        self.update_button.pack(pady=100)
+
+        self.update_window = None
+    
+    def view_selected(self):
+        try:
+            x = trv.selection()
+            self.update_window = customtkinter.CTkToplevel(self)
+            self.update_window.geometry("637x637")
+            self.update_window.focus()
+            self.update_window.resizable(False, False)
+
+            #Finding the data from MongoDB
+            selected_item = collec.find_one({'Department': trv.item(x)["values"][0], 'Date_time': trv.item(x)["values"][1], 'Resort Name': trv.item(x)["values"][2]})
+
+
+
+            #Initializing class UpdateFrame
+            self.my_frame = UpdateFrame(master=self.update_window, resort_name=trv.item(x)["values"][2], amount=trv.item(x)["values"][4], reason=selected_item['Reason'], height=600, width=600)
+            self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        except Exception:
+            print(Exception.__cause__)
+            CTkMessagebox(title='Error', message="Please Select a Row")
+
+
+    def edit_button(self):
+        CTkMessagebox(title='Error', message="You are not allowed to Edit")
+
+
+class MyFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.df = pd.DataFrame(columns=["Department", "Date_time", "Resort Name", "Person Name", "Amount", "Payment_type", "Approved", "Paid"])
+        self.order = True
+
+        try:
+            for one_collec in collec.find():
+                self.df.loc[len(self.df.index)] = [one_collec["Department"], one_collec["Date_time"], one_collec["Resort Name"], one_collec["Person Name"], one_collec["Amount"], one_collec["Payment_type"], one_collec["Approved"], one_collec["Paid"]]
+        except Exception:
+            CTkMessagebox(title="Error", message=Exception)
+
+        self.display_data(self.df)
+        # self.my_tree.pack(pady=10)
+
+        self.style = ttk.Style()
+            
+        self.style.theme_use("default")
+
+        self.style.configure("Treeview",
+                        background="#2a2d2e",
+                        foreground="white",
+                        rowheight=25,
+                        fieldbackground="#343638",
+                        bordercolor="#343638",
+                        borderwidth=0)
+        self.style.map('Treeview', background=[('selected', '#22559b')])
+
+        self.style.configure("Treeview.Heading",
+                        background="#565b5e",
+                        foreground="white",
+                        relief="flat")
+        self.style.map("Treeview.Heading",
+                    background=[('active', '#3484F0')])
+        
+        self.style.configure("Treeview", height=700)
+
+        self.update_window = None
+
+    # Function for sorting column 
+    def sort_column(self, column):
+        if self.order:
+            self.order = False
+        else:
+            self.order = True
+        
+        self.df = self.df.sort_values(by=[column], ascending=self.order)
+        self.display_data(self.df)
+
+    def display_data(self, dataframe):
+        l1 = list(self.df)
+        r_set = self.df.to_numpy().tolist()
+        global trv
+        trv = ttk.Treeview(self, selectmode ='browse', show='headings', height=33, columns=l1)
+        trv.grid(row=1,column=1,padx=30,pady=20)
+        for col in l1:
+            trv.column(col, width = 100, anchor ='c')
+            trv.heading(col, text = col,command=lambda col=col :self.sort_column(col))
+
+        for dt in r_set:  
+            v = [r for r in dt] # creating a list from each row 
+            trv.insert("", 'end' ,values=v) # adding row
+        
