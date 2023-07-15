3ef8bda1ad3a00076d92e2e527413fe9f920755a
Jay Punekar <jaypunekar01@gmail.com>
2023-07-16 03:31:56 +0530

    added a feature to select bank in view tab

--- util.py
+++ util.py
@@ -22,9 +22,9 @@ db = client['Punekar']
 collec = db['Tally']
 
 class UpdateFrame(customtkinter.CTkFrame):
-    def __init__(self, resort_name, amount, reason, master, **kwargs):
+    def __init__(self, client_name, amount, reason, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.resort_name = resort_name
+        self.client_name = client_name
         self.amount = amount
         self.reason = reason
 
@@ -33,8 +33,21 @@ class UpdateFrame(customtkinter.CTkFrame):
 
         self.label_name = customtkinter.CTkLabel(self, text="Resort Name:")
         self.label_name.place(relx=0.15, rely=.2, anchor='nw')
-        self.entry_name = customtkinter.CTkEntry(self, placeholder_text=self.resort_name, width=200)
+        self.entry_name = customtkinter.CTkEntry(self, placeholder_text=self.client_name, width=200)
         self.entry_name.place(relx=.15, rely=.25, anchor='nw')
+
+        # Dropdown menu for Types of Payment
+        self.payment_type = customtkinter.CTkLabel(self, text="Payment Type")
+        self.payment_type.place(relx=0.15, rely=.2, anchor='nw')
+        self.optionmenu = customtkinter.CTkOptionMenu(self ,values=['', 'Cash', 'Bank 1', 'Bank 2', 'Bank 3', 'Bank 4'])
+
+        # This code finds the currently selected
+        self.currently_selected = collec.find_one({"Client Name": str(client_name), "Amount": str(amount), "Reason": str(reason)})
+        if self.currently_selected['Payment_from']:   #if there is a payment type selected that preset the menu to that type or else show blank
+            self.optionmenu.set(self.currently_selected['Payment_from'])
+        else:
+            self.optionmenu.set('')
+        self.optionmenu.place(relx=.55, rely=.25, anchor='nw')
 
         self.label_amount = customtkinter.CTkLabel(self, text="Amount (Rs.)")
         self.label_amount.place(relx=0.15, rely=.3, anchor='nw')
@@ -50,15 +63,30 @@ class UpdateFrame(customtkinter.CTkFrame):
         self.button_save = customtkinter.CTkButton(self, text="Save")
         self.button_save.place(relx=0.3, rely=0.90, anchor='s')
 
-        self.button_update = customtkinter.CTkButton(self, text="Update",)
+        self.button_update = customtkinter.CTkButton(self, text="Update",command=lambda:self.update_bank_details(self.optionmenu.get(), self.client_name, self.amount, self.reason))
         self.button_update.place(relx=0.7, rely=0.90, anchor='s')
+
+    def update_bank_details(self, payment_type, client_name, amount, reason):
+        try:
+            # Convert all the parameter to string so that 'find_one' function will understand
+            # print(collec.find_one({"Client Name": str(client_name), "Amount": str(amount), "Reason": str(reason)}))
+            if payment_type is not '':
+                collec.update_one({"Client Name": str(client_name), "Amount": str(amount), "Reason": str(reason)}, {"$set": {"Payment_from": str(payment_type)}})
+                CTkMessagebox(title='Updated', message="Payment Type has been Succussfully Updated")
+            else:
+                CTkMessagebox(title='Error', message="Please select a proper bank")
+
+
+        except Exception:
+            CTkMessagebox(title='Error', message="There was an Error while updating")
+
 
 
 
 class ButtonFrame(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.update_button = customtkinter.CTkButton(self, text="View")
+        self.update_button = customtkinter.CTkButton(self, text="View", command=self.view_selected)
         self.update_button.pack(pady=10)
         self.update_button = customtkinter.CTkButton(self, text="Delete")
         self.update_button.pack(pady=10)
@@ -82,20 +110,17 @@ class ButtonFrame(customtkinter.CTkFrame):
             self.update_window.resizable(False, False)
 
             #Finding the data from MongoDB
-            selected_item = collec.find_one({'Department': trv.item(x)["values"][0], 'Date_time': trv.item(x)["values"][1], 'Resort Name': trv.item(x)["values"][2]})
+            selected_item = collec.find_one({'Department': trv.item(x)["values"][0], 'Date_time': trv.item(x)["values"][1], 'Client Name': trv.item(x)["values"][2]})
 
 
 
-            #Initializing class UpdateFrame
-            self.my_frame = UpdateFrame(master=self.update_window, resort_name=trv.item(x)["values"][2], amount=trv.item(x)["values"][4], reason=selected_item['Reason'], height=600, width=600)
+            # Initializing class UpdateFrame
+            # UpdateFrame class opens a toplevel frame with all the details in it
+            self.my_frame = UpdateFrame(master=self.update_window, client_name=trv.item(x)["values"][2], amount=trv.item(x)["values"][4], reason=selected_item['Reason'], height=600, width=600)
             self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
         except Exception:
             print(Exception.__cause__)
-            CTkMessagebox(title='Error', message="Please Select a Row")
-
-
-    def edit_button(self):
-        CTkMessagebox(title='Error', message="You are not allowed to Edit")
+            CTkMessagebox(title='Error', message="You haven't selected a row or there was an error in Database")
 
 
 class MyFrame(customtkinter.CTkFrame):
