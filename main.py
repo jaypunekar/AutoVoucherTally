c959a5431b41e44c8c0b06215b60a88bc8dde200
Jay Punekar <jaypunekar01@gmail.com>
2023-07-18 01:16:57 +0530

    added exception handling to all code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import customtkinter
 from CTkMessagebox import CTkMessagebox
 from pymongo.mongo_client import MongoClient
@@ -8,33 +10,41 @@ from PIL import Image
 import requests
 import xml.etree.ElementTree as Et
 from util import UpdateFrame, ButtonFrame, MyFrame
+from exception import TallyException
 
-# Tally URL
-tally_url = "https://localhost:9000"
+try:
+    # Tally URL
+    tally_url = "https://localhost:9000"
 
-# MongoDB Atlas URL
-mongo_url = "mongodb+srv://mongodb:mongodb@tally.i6wfrlt.mongodb.net/?retryWrites=true&w=majority"
+    # MongoDB Atlas URL
+    mongo_url = "mongodb+srv://mongodb:mongodb@tally.i6wfrlt.mongodb.net/?retryWrites=true&w=majority"
 
-client = MongoClient(mongo_url)
+    client = MongoClient(mongo_url)
 
-# Database Name and Collection Name
-db = client['Punekar']
-collec = db['Tally']
+    # Database Name and Collection Name
+    db = client['Punekar']
+    collec = db['Tally']
+except Exception as e:
+        raise TallyException(e, sys) from e
 
 
 class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("950x750")
-        self.grid_rowconfigure(0, weight=1)  # configure grid system
-        self.grid_columnconfigure(0, weight=1)
-        self.resizable(False, False)
+    try:
+        def __init__(self):
+            super().__init__()
+            self.geometry("950x750")
+            self.grid_rowconfigure(0, weight=1)  # configure grid system
+            self.grid_columnconfigure(0, weight=1)
+            self.resizable(False, False)
+            self.title("TallyMain")
 
-        self.my_frame = MyFrame(master=self)
-        self.my_frame.grid(row=0, column=0, padx=10, pady=20, sticky="nsew")
+            self.my_frame = MyFrame(master=self)
+            self.my_frame.grid(row=0, column=0, padx=10, pady=20, sticky="nsew")
 
-        self.button_frame = ButtonFrame(master=self)
-        self.button_frame.grid(row=0, column=1, padx=10, pady=20,sticky='nsew')
+            self.button_frame = ButtonFrame(master=self)
+            self.button_frame.grid(row=0, column=1, padx=10, pady=20,sticky='nsew')
+    except Exception as e:
+        raise TallyException(e, sys) from e
 
 
 app = App()
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import customtkinter
 from CTkMessagebox import CTkMessagebox
 from pymongo.mongo_client import MongoClient
@@ -8,6 +10,7 @@ from PIL import Image
 import requests
 # etree makes it easier to process xml data
 import xml.etree.ElementTree as Et
+from exception import TallyException
 
 # Tally URL
 tally_url = "https://localhost:9000"
@@ -72,8 +75,9 @@ class UpdateFrame(customtkinter.CTkFrame):
             selected_voucher = collec.find_one({"Client Name": str(client_name), "Amount": str(amount), "Reason": str(reason)})
             collec.delete_one({"Client Name": str(client_name), "Amount": str(amount), "Reason": str(reason)})
             CTkMessagebox(title='Updated', message="The voucher is Succussfully Deleated! (Restart the Program to See the Changes)")
-        except Exception:
+        except Exception as e:
             CTkMessagebox(title='Error', message="There was an error while deleating the voucher")
+            raise TallyException(e, sys) from e
 
 
     def update_bank_details(self, payment_type, client_name, amount, reason):
@@ -87,8 +91,9 @@ class UpdateFrame(customtkinter.CTkFrame):
                 CTkMessagebox(title='Error', message="Please select a proper bank")
 
 
-        except Exception:
+        except Exception as e:
             CTkMessagebox(title='Error', message="There was an Error while updating")
+            raise TallyException(e, sys) from e
         
 
 class ButtonFrame(customtkinter.CTkFrame):
@@ -110,9 +115,10 @@ class ButtonFrame(customtkinter.CTkFrame):
     def view_selected(self):
         try:
             x = trv.selection()
-            print(x)
+            # print(x)
             self.update_window = customtkinter.CTkToplevel(self)
             self.update_window.geometry("637x637")
+            self.update_window.title("Update Voucher")
             self.update_window.focus()
             self.update_window.resizable(False, False)
 
@@ -124,16 +130,20 @@ class ButtonFrame(customtkinter.CTkFrame):
             # UpdateFrame class opens a toplevel frame with all the details in it
             self.my_frame = UpdateFrame(master=self.update_window, client_name=trv.item(x)["values"][2], amount=trv.item(x)["values"][4], reason=self.selected_item['Reason'], height=600, width=600)
             self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
-        except Exception:
-            print(Exception.__cause__)
+        except Exception as e:
+            # print(Exception.__cause__)
             CTkMessagebox(title='Error', message="You haven't selected a row or there was an error in Database")
+            raise TallyException(e, sys) from e
 
 
     def integrate_in_tally(self):
-        # try:
+        try:
             all_items = collec.find({"Paid": 1, "Approved": 1, "Added_to_tally": 0})
             #Tally server Runs on localhost:9000 by default
             self.url = "http://localhost:9000/"
+
+            if all_items == None:
+                CTkMessagebox(title='Error', message="All the Items have been added to Tally")
 
             for items in all_items:
                 print(items["Reason"])
@@ -160,12 +170,12 @@ class ButtonFrame(customtkinter.CTkFrame):
                                     <VOUCHERTYPENAME>Payment</VOUCHERTYPENAME>
                                     <VOUCHERNUMBER>1</VOUCHERNUMBER>
                                     <ALLLEDGERENTRIES.LIST>
-                                        <LEDGERNAME>Sneh Readsfsort</LEDGERNAME>
+                                        <LEDGERNAME>Sneh Resort</LEDGERNAME>
                                         <ISDEEMEDPOSITIVE>Yes</ISDEEMEDPOSITIVE>
                                         <AMOUNT>{int(items["Amount"])}</AMOUNT>
                                     </ALLLEDGERENTRIES.LIST>
                                     <ALLLEDGERENTRIES.LIST>
-                                        <LEDGERNAME>Indian Badnk</LEDGERNAME>
+                                        <LEDGERNAME>Indian Bank</LEDGERNAME>
                                         <ISDEEMEDPOSITIVE>No</ISDEEMEDPOSITIVE>
                                         <AMOUNT>{-int(items["Amount"])}</AMOUNT>
                                     </ALLLEDGERENTRIES.LIST>
@@ -191,72 +201,85 @@ class ButtonFrame(customtkinter.CTkFrame):
                     else:
                         CTkMessagebox(title='Error', message="The Payment Method or the client does not exist in Tally yet. Please add it and try again.")
                         print(type(responseXML[1][0][0][0].text))
-                except Exception:
+                except Exception as e:
                         CTkMessagebox(title='Error', message="The Payment Method or the client does not exist in Tally yet. Please add it and try again.")
-            # except Exception:
-            #     CTkMessagebox(title='Error', message="Tally is not open in background")
+                        raise TallyException(e, sys) from e
+        except Exception as e:
+            CTkMessagebox(title='Error', message="Tally is not open in background")
+            raise TallyException(e, sys) from e
 
 
 class MyFrame(customtkinter.CTkFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-        self.df = pd.DataFrame(columns=["Department", "Date_time", "Client Name", "Person Name", "Amount", "Payment_to", "Approved", "Paid"])
-        self.order = True
+    try:
+        def __init__(self, master, **kwargs):
+            super().__init__(master, **kwargs)
+            self.df = pd.DataFrame(columns=["Department", "Date_time", "Client Name", "Person Name", "Amount", "Payment_to", "Approved", "Paid"])
+            self.order = True
 
-        try:
-            for one_collec in collec.find():
-                self.df.loc[len(self.df.index)] = [one_collec["Department"], one_collec["Date_time"], one_collec["Client Name"], one_collec["Person Name"], one_collec["Amount"], one_collec["Payment_to"], one_collec["Approved"], one_collec["Paid"]]
-        except Exception:
-            CTkMessagebox(title="Error", message="There is a error in MyFrame")
+            try:
+                for one_collec in collec.find():
+                    self.df.loc[len(self.df.index)] = [one_collec["Department"], one_collec["Date_time"], one_collec["Client Name"], one_collec["Person Name"], one_collec["Amount"], one_collec["Payment_to"], one_collec["Approved"], one_collec["Paid"]]
+            except Exception as e:
+                CTkMessagebox(title="Error", message="There is a error in MyFrame")
+                raise TallyException(e, sys) from e
 
-        self.display_data(self.df)
-        # self.my_tree.pack(pady=10)
+            self.display_data(self.df)
+            # self.my_tree.pack(pady=10)
 
-        self.style = ttk.Style()
+            self.style = ttk.Style()
+                
+            self.style.theme_use("default")
+
+            self.style.configure("Treeview",
+                            background="#2a2d2e",
+                            foreground="white",
+                            rowheight=25,
+                            fieldbackground="#343638",
+                            bordercolor="#343638",
+                            borderwidth=0)
+            self.style.map('Treeview', background=[('selected', '#22559b')])
+
+            self.style.configure("Treeview.Heading",
+                            background="#565b5e",
+                            foreground="white",
+                            relief="flat")
+            self.style.map("Treeview.Heading",
+                        background=[('active', '#3484F0')])
             
-        self.style.theme_use("default")
+            self.style.configure("Treeview", height=700)
 
-        self.style.configure("Treeview",
-                        background="#2a2d2e",
-                        foreground="white",
-                        rowheight=25,
-                        fieldbackground="#343638",
-                        bordercolor="#343638",
-                        borderwidth=0)
-        self.style.map('Treeview', background=[('selected', '#22559b')])
-
-        self.style.configure("Treeview.Heading",
-                        background="#565b5e",
-                        foreground="white",
-                        relief="flat")
-        self.style.map("Treeview.Heading",
-                    background=[('active', '#3484F0')])
-        
-        self.style.configure("Treeview", height=700)
-
-        self.update_window = None
+            self.update_window = None
+    except Exception as e:
+        raise TallyException(e, sys) from e
 
     # Function for sorting column 
     def sort_column(self, column):
-        if self.order:
-            self.order = False
-        else:
-            self.order = True
-        
-        self.df = self.df.sort_values(by=[column], ascending=self.order)
-        self.display_data(self.df)
+        try:
+            if self.order:
+                self.order = False
+            else:
+                self.order = True
+            
+            self.df = self.df.sort_values(by=[column], ascending=self.order)
+            self.display_data(self.df)
+        except Exception as e:
+            raise TallyException(e, sys) from e
 
     def display_data(self, dataframe):
-        l1 = list(self.df)
-        r_set = self.df.to_numpy().tolist()
-        global trv
-        trv = ttk.Treeview(self, selectmode ='browse', show='headings', height=33, columns=l1)
-        trv.grid(row=1,column=1,padx=30,pady=20)
-        for col in l1:
-            trv.column(col, width = 100, anchor ='c')
-            trv.heading(col, text = col,command=lambda col=col :self.sort_column(col))
+        try:
+            l1 = list(self.df)
+            r_set = self.df.to_numpy().tolist()
+            global trv
+            trv = ttk.Treeview(self, selectmode ='browse', show='headings', height=33, columns=l1)
+            trv.grid(row=1,column=1,padx=30,pady=20)
+            for col in l1:
+                trv.column(col, width = 100, anchor ='c')
+                trv.heading(col, text = col,command=lambda col=col :self.sort_column(col))
 
-        for dt in r_set:  
-            v = [r for r in dt] # creating a list from each row 
-            trv.insert("", 'end' ,values=v) # adding row
+            for dt in r_set:  
+                v = [r for r in dt] # creating a list from each row 
+                trv.insert("", 'end' ,values=v) # adding row
+        except Exception as e:
+            raise TallyException(e, sys) from e
+
         
